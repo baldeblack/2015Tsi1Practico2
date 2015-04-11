5769fffc62febd58db45b4860b65a03bdd9b4e22
baldeblack <gbg933@gmail.com>
2015-04-11 11:01:16 -0300

    se agrega mvc

---
+++ PresentationLayerMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PresentationLayerMvc.Startup))]
+namespace PresentationLayerMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
